0b5a486738feb3ef93e53e9d0403011c37cd4294
akilandeshwari651-a11y <akilandeshwari651@gmail.com>
2026-06-30 22:48:39 +0530

    Add showMessage function for birthday greeting
    Adds a function to display a birthday message.

---
+++ script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+    "🎉 Happy Birthday, Atchaya! ❤️<br><br>" +
+    "May your smile always shine bright, your dreams come true, and your life be filled with happiness, love, success, and endless beautiful memories. 🥳🎂✨";
+}
