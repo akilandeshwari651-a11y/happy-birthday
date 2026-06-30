9b74cf3077f6a70ad48639f3d3beec7130044c8f
akilandeshwari651-a11y <akilandeshwari651@gmail.com>
2026-06-30 23:01:48 +0530

    Update birthday message and photo slideshow

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+const photos = [
+    "IMG-20260630-WA0005.jpg",
+    "IMG-20260630-WA0006.jpg",
+    "IMG-20260630-WA0010.jpg",
+    "IMG-20260630-WA0015.jpg",
+    "IMG-20260630-WA0016.jpg",
+    "IMG-20260630-WA0053.jpg"
+];
+
+let i = 0;
+
+setInterval(() => {
+    i = (i + 1) % photos.length;
+    document.getElementById("slide").src = photos[i];
+}, 2500);
+
 function showMessage() {
     document.getElementById("message").innerHTML =
-    "🎉 Happy Birthday, Atchaya! ❤️<br><br>" +
-    "May your smile always shine bright, your dreams come true, and your life be filled with happiness, love, success, and endless beautiful memories. 🥳🎂✨";
+    "🎉 Happy Birthday Atchaya ❤️<br><br>May your life be filled with happiness, love, success and endless smiles! 🥳";
 }
